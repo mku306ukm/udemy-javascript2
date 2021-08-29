307ccba28c59802694a3b1ea5d00c3c2a59d6db4
umesh_kumar_mehta <kumarumesh15294@gmail.com>
2021-08-29 05:12:24 -0700

    f2-coding-challenge4

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 
 
-// The while Loop
-for (let rep = 1; rep <= 10; rep++) {
-  console.log(`Lifting weights repetition ${rep} `);
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
-let rep = 1;
-while (rep <= 10) {
-  
-  rep++;
+const bills = [50, 50, 50, 50, 50, 50, 50, 50, 50, 50];
+const tips = [];
+const totals = [];
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
 }
-let dice = Math.trunc(Math.random() * 6) + 1;
-while (dice !== 6) {
-  console.log(`You rolled a ${dice}`);
-  dice = Math.trunc(Math.random() * 6) + 1;
-  if (dice === 6) console.log('Loop is about to end...');
+console.log(bills, tips, totals);
+
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
 }
+console.log(calcAverage([10,30,50]));
+ console.log(calcAverage(totals));
+ console.log(calcAverage(tips));
