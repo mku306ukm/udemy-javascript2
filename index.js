f6b2b1ccda607a94f783cc139b302497936be188
umesh_kumar_mehta <kumarumesh15294@gmail.com>
2021-08-29 03:43:52 -0700

    the-while-loop

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 
- //Looping Backwards and Loops in Loops
- const pipra = [
-  'umesh',
-  'kumar',
-  2021-1994,
-  'learner',
-  ['brajesh', 'samod', 'manoj'],
-  true
-];
-// 0, 1, ..., 4
-// 4, 3, ..., 0
-for (let i = pipra.length - 1; i >= 0; i--) {
-  console.log(i, pipra[i]);
+
+// The while Loop
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} `);
 }
-
-
-for (let exercise = 1; exercise < 4; exercise++) {
-  console.log(`--- Starting exercise ${exercise}`);
-  for (let rep = 1; rep < 6; rep++) {
-   console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} `);
-  }
- }
+let rep = 1;
+while (rep <= 10) {
+  
+  rep++;
+}
+let dice = Math.trunc(Math.random() * 6) + 1;
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log('Loop is about to end...');
+}
