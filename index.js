e3ae8d5079ef2a689264631ae8739bf6b02183db
umesh_kumar_mehta <kumarumesh15294@gmail.com>
2021-08-28 23:11:55 -0700

    looping-backwards-and-loops-in-loops

--- index.js
+++ index.js
@@ -1,39 +1,23 @@
 
+ //Looping Backwards and Loops in Loops
+ const pipra = [
+  'umesh',
+  'kumar',
+  2021-1994,
+  'learner',
+  ['brajesh', 'samod', 'manoj'],
+  true
+];
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+for (let i = pipra.length - 1; i >= 0; i--) {
+  console.log(i, pipra[i]);
+}
 
-// Looping Arrays, Breaking and Continuing
-const  pipra = [
-    'umesh',
-    'kumar',
-    2021 - 1994,
-    'teacher',
-    ['brajesh', 'samod', 'gopal'],
-    true
-  ];
-  const types = [];
-  
-  for (let i = 0; i <  pipra.length; i++) {
-    
-    console.log( pipra[i], typeof  pipra[i]);
-    
-    types.push(typeof  pipra[i]);
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`--- Starting exercise ${exercise}`);
+  for (let rep = 1; rep < 6; rep++) {
+   console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} `);
   }
-  console.log(types);
-  
-  const years = [1991, 1992, 1993, 1994];
-  const ages = [];
-  for (let i = 0; i < years.length; i++) {
-    ages.push(2021 - years[i]);
-  }
-  console.log(ages);
-  // continue and break
-  
-  console.log('--- ONLY STRINGS ---')
-  for (let i = 0; i <  pipra.length; i++) {
-    if (typeof  pipra[i] !== 'string') continue;
-    console.log( pipra[i], typeof  pipra[i]);
-  }
-  console.log('--- BREAK WITH NUMBER ---')
-  for (let i = 0; i <  pipra.length; i++) {
-    if (typeof  pipra[i] === 'number') break;
-    console.log( pipra[i], typeof  pipra[i]);
-  }
+ }
