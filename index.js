3c581808b56001b274351055d74ea48ad726d8d8
umesh_kumar_mehta <kumarumesh15294@gmail.com>
2021-09-01 01:31:45 -0700

    guess-my-number-1

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-
-
-
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-}
-const bills = [50, 50, 50, 50, 50, 50, 50, 50, 50, 50];
-const tips = [];
-const totals = [];
-for (let i = 0; i < bills.length; i++) {
-  const tip = calcTip(bills[i]);
-  tips.push(tip);
-  totals.push(tip + bills[i]);
-}
-console.log(bills, tips, totals);
-
-
-const calcAverage = function (arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    // sum = sum + arr[i];
-    sum += arr[i];
-  }
-  return sum / arr.length;
-}
-console.log(calcAverage([10,30,50]));
- console.log(calcAverage(totals));
- console.log(calcAverage(tips));
+'use strict';
+console.log(document.querySelector('.message').textContent);
+console.log(document.querySelector('.between'));
+console.log(document.querySelector('.number').textContent);
